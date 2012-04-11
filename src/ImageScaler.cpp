b6dd039e5cd8d1e1494b88730c4f9c55c2ee16e6
Ovilia <zwl.sjtu@gmail.com>
2012-04-11 15:39:18 +0800

    resolve a bug in bilinear

--- src/ImageScaler.cpp
+++ src/ImageScaler.cpp
@@ -54,15 +54,15 @@ QImage* ImageScaler::getBilinearImage(QImage* originImage,
     int oHeight = originImage->height();
     for (int i = 0; i < newHeight; ++i) {
         for (int j = 0; j < newWidth; ++j) {
-            float x = (float)j * oWidth / newWidth;
-            float y = (float)i * oHeight / newHeight;
+            float x = (float)j * (oWidth - 1.0) / (newWidth - 1.0);
+            float y = (float)i * (oHeight - 1.0) / (newHeight - 1.0);
             int x1 = (int)x;
             int y1 = (int)y;
             const uchar* const q11 = oBits + (y1 * oWidth + x1) * 4;
             // check if is the end of a line, if so, don't use the next line
-            const uchar* const q21 = (j == newWidth - 1) ? q11 : (q11 + 4);
-            const uchar* const q12 = (i == newHeight - 1) ? q11 : (q11 + oWidth * 4);
-            const uchar* const q22 = (j == newWidth - 1) ? q12 : (q12 + 4);
+            const uchar* const q21 = (x1 >= oWidth - 1) ? q11 : (q11 + 4);
+            const uchar* const q12 = (y1 >= oHeight - 1) ? q11 : (q11 + oWidth * 4);
+            const uchar* const q22 = (x1 >= oWidth - 1) ? q12 : (q12 + 4);
 
             for (int rgb = 0; rgb < 3; ++rgb) {
                 *newBits = (uchar)(*(q11 + rgb) * (x1 + 1 - x) * (y1 + 1 - y) +
